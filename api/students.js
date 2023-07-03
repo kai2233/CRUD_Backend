cdb71361be298a18979e97b40d27ee8f2411299c
Kaifeng <1536771545kai@gmail.com>
2023-07-03 15:02:38 -0400

    upload update student feature

--- api/students.js
+++ api/students.js
@@ -56,4 +56,14 @@ router.get("/findStudent/:id", async (req, res, next) => {
     }
   });
 
+  router.put("/updateStudent", async (req, res, next) => {
+    try{
+      const targetStudent = await students.findOne({where: { id: req.body.id }});
+      await targetStudent.update(req.body);
+      res.status(200).json(targetStudent);
+    }catch (err) {
+      next(err);
+    }
+  });
+
 module.exports = router;
